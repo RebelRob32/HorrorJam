b22753c9c805d91e48ec9f4c8359aa34ee2ccbe0
RebelRob32 <r.gue89@protonmail.com>
2022-03-26 10:05:14 -0500

    Today's Updates
    Updated the follow function, how the scene transitions to Wonderland, some lighting, added a Soothe function with a number of Charges

--- Alice in Wonderland Jam/Assets/Scripts/WonderlandTransition.cs
+++ Alice in Wonderland Jam/Assets/Scripts/WonderlandTransition.cs
@@ -7,15 +7,45 @@ public class WonderlandTransition : MonoBehaviour
     public GameManager gm;
     public AliceController alice;
     public RabbitController rabbit;
+    public GameObject wonderlandPortal;
 
     public void Awake()
     {
         gm = GameObject.FindObjectOfType<GameManager>();
+        alice = GameObject.FindObjectOfType<AliceController>();
+        rabbit = GameObject.FindObjectOfType<RabbitController>();
+    }
+
+    public void Update()
+    {
+        if(wonderlandPortal == null)
+        {
+            return;
+        }
+
+        if (alice.fearLevel <= 50f && rabbit.isClose == true)
+        {
+            bool isActive = wonderlandPortal.activeSelf;
+            wonderlandPortal.SetActive(true);
+        }
+        else
+        if(rabbit.isClose == false)
+        {
+            wonderlandPortal.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Rabbit" && alice.fearLevel <= 50f && rabbit.isClose == true)
+        {
+            gm.MoveToWonderland();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Rabbit" && alice.fearLevel <= 50f && rabbit.isClose == true)
         {
             gm.MoveToWonderland();
         }
